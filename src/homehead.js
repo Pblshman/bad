e29813d1c8efff75b0f1ba3462507d6e48f71b0e
Pblshman <duiberhernando@hotmail.com>
2024-01-19 18:53:06 -0500

    other comit

--- src/homehead.js
+++ src/homehead.js
@@ -3,13 +3,7 @@ import cont1 from "./cont1";
 
 function homeHead() {
     const homeHeed = addHead("HOME")
-    let running = 1
-    if (running == 1){
-      
-      document.getElementById("contentText").replaceChild(cont1(), document.getElementById("contentText").firstChild)
-      homeHeed.style.pointerEvents = "none"
-
-    }
+  
     homeHeed.onclick = function() {
       
         document.getElementById("contentText").replaceChild(cont1(), document.getElementById("contentText").firstChild)
